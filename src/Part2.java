d28338ac8054fda3fe39d8850d367164a4512fbc
reny1 <reny1@tcd.ie>
2022-04-10 16:01:06 +0100

    Update part2 test

--- src/Part2.java
+++ src/Part2.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class Part2 {
+    public static void main(String[] args) {
+        SearchByName nameSearch = new SearchByName("stops.txt");
+        Scanner input = new Scanner(System.in);
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("Please enter the name(in UPPERCASE), or enter 'quit' to exit: ");
+            if (input.hasNext()) {
+                String name = input.next();
+                nameSearch.search(name);
+                if (!nameSearch.id.isEmpty()) {
+                    System.out.printf("The stops that includes '%s' is as below:\n", name);
+                    System.out.println("-----------------------------------------------------------------------------" +
+                            "----------------------------------------------------------------------------------------");
+                    System.out.printf("%10s %10s %45s %45s %15s %15s %12s %10s %15s %18s\n", "STOP ID", "STOP CODE",
+                            "STOP NAME", "STOP DESC", "STOP LAT", "STOP LON", "ZONE ID", "STOP URL",
+                            "LOCATION TYPE", "PARENT STATION");
+                    System.out.println("-----------------------------------------------------------------------------" +
+                            "----------------------------------------------------------------------------------------");
+                    for(int i = 0; i < nameSearch.id.size(); i++){
+                        System.out.format("%10s %10s %45s %45s %15s %15s %12s %10s %15s %18s\n", nameSearch.id.get(i),
+                                nameSearch.s_code.get(i), nameSearch.s_name.get(i), nameSearch.s_desc.get(i),
+                                nameSearch.s_lat.get(i), nameSearch.s_lon.get(i), nameSearch.zone.get(i),
+                                nameSearch.s_url.get(i), nameSearch.loc_type.get(i), nameSearch.par_station.get(i));
+                    }
+                    System.out.println("-----------------------------------------------------------------------------" +
+                            "----------------------------------------------------------------------------------------");
+                } else if (name.equals("quit")) {
+                    exit = true;
+                } else {
+                    System.out.println("No stops found");
+                }
+            }
+        }
+    }
 }
--- src/SearchByName.java
+++ src/SearchByName.java
@@ -1,2 +1,107 @@
+import TST.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SearchByName {
+    private String fileName;
+    private TST<String> tree;
+    public ArrayList<String> id, s_code, s_name, s_desc, s_lat, s_lon, zone, s_url, loc_type, par_station;
+
+    SearchByName(String stops){
+        this.fileName = stops;
+        this.tree = new TST<String>();
+        readFile();
+    }
+
+    private void readFile(){
+        try{
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader buffer = new BufferedReader(fileReader);
+            boolean endOfFile = false;
+            buffer.readLine();
+            while(!endOfFile){
+                String data = buffer.readLine();
+                if (data != null){
+                    String[] dataArray = data.trim().split(",");
+                    tree.put(formatWords(dataArray[2]), dataArray[0]);
+                }
+                else{
+                    endOfFile = true;
+                }
+            }
+            buffer.close();
+            fileReader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private String formatWords(String words){
+        String[] wordsArray = words.trim().split("\\s+", 2);
+        if(wordsArray[0].equals("WB") || wordsArray[0].equals("NB") || wordsArray[0].equals("SB")|| wordsArray[0].equals("EB")){
+            words = wordsArray[1] + " " + wordsArray[0];
+        }
+        return words;
+    }
+
+    public void search(String inputName){
+        this.id = new ArrayList<>();
+        this.s_code = new ArrayList<>();
+        this.s_name = new ArrayList<>();
+        this.s_desc = new ArrayList<>();
+        this.s_lat = new ArrayList<>();
+        this.s_lon = new ArrayList<>();
+        this.zone = new ArrayList<>();
+        this.s_url = new ArrayList<>();
+        this.loc_type = new ArrayList<>();
+        this.par_station = new ArrayList<>();
+
+        Iterable<String> queue = tree.keysWithPrefix(inputName);
+        String stop_id;
+        if(queue.iterator().hasNext()) {
+            for (String s : queue) {
+                stop_id = tree.get(s);
+                findInfo(stop_id);
+            }
+        }
+    }
+
+    private void findInfo(String stop_id){
+        try{
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader buffer = new BufferedReader(fileReader);
+            boolean endOfFile = false;
+            buffer.readLine();
+            while(!endOfFile){
+                String data = buffer.readLine();
+                if (data != null){
+                    String[] dataArray = data.trim().split(",");
+                    if(dataArray[0].equals(stop_id)){
+                        id.add(dataArray[0]);
+                        s_code.add(dataArray[1]);
+                        s_name.add(dataArray[2]);
+                        s_desc.add(dataArray[3]);
+                        s_lat.add(dataArray[4]);
+                        s_lon.add(dataArray[5]);
+                        zone.add(dataArray[6]);
+                        s_url.add(dataArray[7]);
+                        loc_type.add(dataArray[8]);
+                        par_station.add((dataArray.length == 9) ? " " : dataArray[9]);
+                        endOfFile = true;
+                    }
+                }
+                else{
+                    endOfFile = true;
+                }
+            }
+            buffer.close();
+            fileReader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
